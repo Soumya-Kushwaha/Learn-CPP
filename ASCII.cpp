9fe56210b873d26b35d36d40604cdc9dd7ab4035
Soumya Kushwaha <kushsoumya30@gmail.com>
2022-07-29 09:05:51 +0530

    Add files via upload

---
+++ ASCII.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    char a;
+    std::cout<<"Please enter a character: ";
+    std::cin>>a;
+    std::cout<<"the ASCII value of "<<a<<" is: "<<int(a);
+    return 0;
+}
